ab89fd9bded5726944912ce3d0027923091ef835
Viktor <viktor.sydorenko@gmail.com>
2013-02-16 21:43:10 +0100

    Prepared initial view controller

--- BuildAnywhere/MainNavController.h
+++ BuildAnywhere/MainNavController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  MainNavController.h
 //  BuildAnywhere
 //
 //  Created by Viktor Sydorenko on 2/16/13.
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface MainNavController : UINavigationController
 
 @end
---
+++ BuildAnywhere/MainNavController.m
@@ -0,0 +1,38 @@
+//
+//  MainNavController.m
+//  BuildAnywhere
+//
+//  Created by Viktor Sydorenko on 2/16/13.
+//  Copyright (c) 2013 Viktor Sydorenko. All rights reserved.
+//
+
+#import "MainNavController.h"
+
+@interface MainNavController ()
+
+@end
+
+@implementation MainNavController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
