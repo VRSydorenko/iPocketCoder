78563a38ad61a5368ef1bfb5d7cd04b7ec27eae2
Viktor <viktor.sydorenko@gmail.com>
2013-02-16 18:43:28 +0100

    Initial Commit

---
+++ BuildAnywhere/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BuildAnywhere
+//
+//  Created by Viktor Sydorenko on 2/16/13.
+//  Copyright (c) 2013 Viktor Sydorenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
