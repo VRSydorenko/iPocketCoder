24e0a25f6e00b0cdd5a56454ee69d4ab187c58c5
Viktor Sydorenko <viktor.sydorenko@gmail.com>
2014-01-23 22:18:40 +0100

    Code samples fixed

--- Data/RunManager.h
+++ Data/RunManager.h
@@ -10,10 +10,10 @@
 #import "ideoneIdeone_Service_v1Service.h"
 #import "Project.h"
 
-#define USER @"pocketcoder"
-#define PASS @"ideonepocketcoderapi"
-//#define USER @"ideonevrs"
-//#define PASS @"ideonevrsapi"
+//#define USER @"pocketcoder"
+//#define PASS @"ideonepocketcoderapi"
+#define USER @"ideonevrs"
+#define PASS @"ideonevrsapi"
 
 typedef enum ErrorCodes{
     OK              = 0,
